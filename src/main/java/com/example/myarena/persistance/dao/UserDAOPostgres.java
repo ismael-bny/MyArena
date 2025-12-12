67e87616e564497f2dc181e21441704ca80b3e23
Syrine <benalisyrine8@gmail.com>
2025-12-12 16:11:41 +0100

    port modification for the database. configuration on the port 5430

--- src/main/java/com/example/myarena/persistance/dao/UserDAOPostgres.java
+++ src/main/java/com/example/myarena/persistance/dao/UserDAOPostgres.java
@@ -8,7 +8,7 @@ import java.sql.*;
 
 public class UserDAOPostgres implements UserDAO {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/myarena";
+    private static final String URL = "jdbc:postgresql://localhost:5430/myarena";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
 
